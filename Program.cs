055d407fe4a2e495ae3a7deb371f599fbe07ffe3
Alexander Kvasnikov <a.kvasnikov@carx-tech.com>
2025-01-23 17:19:23 +0300

    2.1 first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 3;
+            a = b;
+
+            int f;
+            f = a + b;
+
+            string s = "hello world";
+
+            float t = 7.8f;
+            double j = 5.8457;
+
+            char c = 'H';
+
+            bool bl = true;
+
+            byte k;
+            sbyte sb;
+            short sh;
+            ushort ush;
+            uint ui;
+            long l;
+        }
+    }
+}
